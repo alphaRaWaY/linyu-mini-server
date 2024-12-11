be8b8ec155f6bf6476b8e40202ec0792fb9440c8
Heath <duweihengmr@163.com>
2024-12-11 20:35:49 +0800

    初始化提交

---
+++ src/main/java/com/cershy/linyuminiserver/LinyuMiniServerApplication.java
@@ -0,0 +1,13 @@
+package com.cershy.linyuminiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinyuMiniServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinyuMiniServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cershy/linyuminiserver/LinyuMiniServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cershy.linyuminiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinyuMiniServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
